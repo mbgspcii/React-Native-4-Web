43f0e84c54029b9b11317f5a2e83d1614c99943d
Miguel Beignon <mbgspcii@gmail.com>
2022-09-20 15:13:42 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+
+const logoUri = `https://demo.twic.pics/components/flip/orange-1.jpg?twic=v1/focus=auto/cover=300x300`;
+
+function Link(props) {
+  return (
+    <Text
+      {...props}
+      accessibilityRole="link"
+      style={StyleSheet.compose(styles.link, props.style)}
+    />
+  );
+}
+
+function App() {
+  return (
+    <View style={styles.app}>
+      <View style={styles.header}>
+        <Image
+          accessibilityLabel="React logo"
+          source={{ uri: logoUri }}
+          resizeMode="contain"
+          style={styles.logo}
+        />
+        <Text style={styles.title}>React Native for Web</Text>
+      </View>
+      <Text style={styles.text}>
+        This is an example of an app built with{" "}
+        <Link href="https://github.com/facebook/create-react-app">
+          Create React App
+        </Link>{" "}
+        and{" "}
+        <Link href="https://github.com/necolas/react-native-web">
+          React Native for Web
+        </Link>
+      </Text>
+      <Text style={styles.text}>
+        To get started, edit{" "}
+        <Link href="https://codesandbox.io/s/q4qymyp2l6/" style={styles.code}>
+          src/App.js
+        </Link>
+        .
+      </Text>
+      <Button onPress={() => {}} title="Example button" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  app: {
+    marginHorizontal: "auto",
+    maxWidth: 500
+  },
+  logo: {
+    height: 300
+  },
+  header: {
+    padding: 20
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: "1.5rem",
+    marginVertical: "1em",
+    textAlign: "center"
+  },
+  text: {
+    lineHeight: "1.5em",
+    fontSize: "1.125rem",
+    marginVertical: "1em",
+    textAlign: "center"
+  },
+  link: {
+    color: "#1B95E0"
+  },
+  code: {
+    fontFamily: "monospace, monospace"
+  }
+});
+
+export default App;
